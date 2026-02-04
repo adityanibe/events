f66ceefd160e505386893d4a75f187189f1bcf48
adityanibe <165752157+adityanibe@users.noreply.github.com>
2026-02-04 20:17:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let btn=document.querySelector("#btn")
 
 btn.addEventListener("mouseenter", clickfun=()=>{
     // alert("form submitted successfully")
-    window.location.href =( "https://www.youtube.com")
+    window.location.href =( "https://www.hostinger.com/in/domain-name-search")
     // window.open("https://www.youtube.com", "_blank")
 })
 
 
 // console.log(btn)
+
