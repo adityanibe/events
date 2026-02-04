6c4d535dd98ab4292e5e35408f13ef0a7df8529a
adityanibe <165752157+adityanibe@users.noreply.github.com>
2026-02-04 20:14:21 +0530

    Modify mouseenter event to redirect to YouTube
    Change event action on mouseenter to redirect instead of opening a new tab.

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let btn=document.querySelector("#btn")
 
 btn.addEventListener("mouseenter", clickfun=()=>{
     // alert("form submitted successfully")
-    // window.location.href =( "https://www.youtube.com")
-    window.open("https://www.youtube.com", "_blank")
+    window.location.href =( "https://www.youtube.com")
+    // window.open("https://www.youtube.com", "_blank")
 })
+
 
 // console.log(btn)
