95e5b7d67df4ce882711cc5f577f2dc54bc2fb40
adityanibe <165752157+adityanibe@users.noreply.github.com>
2026-02-04 20:11:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn=document.querySelector("#btn")
+
+btn.addEventListener("mouseenter", clickfun=()=>{
+    // alert("form submitted successfully")
+    // window.location.href =( "https://www.youtube.com")
+    window.open("https://www.youtube.com", "_blank")
+})
+
+// console.log(btn)
